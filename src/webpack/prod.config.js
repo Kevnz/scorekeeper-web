26779b74323077ffea372a90e3974f3ba162c329
Kevin Isom <kevin.isom@gmail.com>
2019-05-05 02:30:31 +1200

    icons and copy

--- src/webpack/prod.config.js
+++ src/webpack/prod.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 const baseConfig = require('./config')
 const prodConfig = {
   plugins: [
@@ -16,6 +17,12 @@ const prodConfig = {
       template: './src/ui/index.html',
       historyApiFallback: true,
     }),
+    new CopyPlugin([
+      {
+        from: path.join(process.cwd(), '/src', '/public'),
+        to: path.join(process.cwd(), '/dist'),
+      },
+    ]),
   ],
   output: {
     path: path.join(process.cwd(), '/dist'),
