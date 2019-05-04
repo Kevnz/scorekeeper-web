64023ead38513dd3bd906ed4a14f54e73330ce7f
Kevin Isom <kevin.isom@gmail.com>
2019-05-04 20:54:03 +1200

    No log

--- src/ui/index.js
+++ src/ui/index.js
@@ -4,7 +4,7 @@ import 'mini.css'
 import App from './core/app'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-console.log('pro', process.env.NODE_ENV)
+
 if (process.env.NODE_ENV !== 'production') {
   module.hot.accept()
 }
