c3ac199af9a672c12855ed18d507cc9ad29ba15c
Kevin Isom <kevin.isom@gmail.com>
2019-06-06 07:33:38 +1200

    No log

--- src/ui/reducers/index.js
+++ src/ui/reducers/index.js
@@ -1,7 +1,7 @@
 import quarter from './quarter'
 import scoring from './scoring'
 import game from './game'
-console.log('game', game)
+
 const reducer = (state, action, ...funcs) =>
   funcs.reduce((inProgress, current) =>
     current({ ...state, ...inProgress }, action)
@@ -10,10 +10,9 @@ const noop = (state, action) => {
   return { state, action }
 }
 const composer = (...funcs) => {
-  console.log('funcs', funcs)
   return (state, action) => reducer(state, action, ...funcs)
 }
 
 const composed = composer(noop, game, scoring, quarter)
-console.log('composed', composed)
+
 export default composed
