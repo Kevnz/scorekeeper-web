bef9e5f8cf939b51dca3188c7861bcddc8eb7119
Kevin Isom <kevin.isom@gmail.com>
2019-08-31 10:24:09 +1200

    Updates

--- 2.bundle.js
+++ 2.bundle.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{48:function(n,e,t){"use strict";t.r(e);var a=t(0),u=t.n(a);e.default=function(){return u.a.createElement("main",null,u.a.createElement("h1",null,"About"))}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{143:function(n,e,t){"use strict";t.r(e);var a=t(0),u=t.n(a);e.default=function(){return u.a.createElement("main",null,u.a.createElement("h1",null,"About"))}}}]);
